be090dd1177ed200efc9c45a231aee9c1162bd90
VENZMARL <166726630+VENZMARL@users.noreply.github.com>
2024-06-10 20:54:11 +0800

    Added element at script.js

---
+++ src/Name.js
@@ -0,0 +1,10 @@
+function New() {
+    return(
+        <>
+        <h1>My Full name: Marlon Vencito</h1>
+        </>
+    )
+}
+
+
+export default New;
---
+++ src/New.js
@@ -0,0 +1,13 @@
+
+
+
+function New() {
+    return(
+        <>
+        <h1>This component is a new component</h1>
+        </>
+    )
+}
+
+
+export default New;
